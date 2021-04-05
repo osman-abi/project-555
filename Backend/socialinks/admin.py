6b4461abf1153e9c40976606c336d33920c9580c
osman-abi <osman.97.abi@gmail.com>
2021-04-05 15:56:43 +0400

    test

--- Backend/socialinks/admin.py
+++ Backend/socialinks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import FacebookLink, InstagramLink, WhatsappLink
+
+admin.site.register(FacebookLink)
+
+admin.site.register(InstagramLink)
+
+admin.site.register(WhatsappLink)
 
 # Register your models here.
