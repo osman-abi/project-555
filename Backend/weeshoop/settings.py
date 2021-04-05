5f7ab549bf506d486f919d053aa40ade34e24f51
osman-abi <osman.97.abi@gmail.com>
2021-04-05 14:45:04 +0400

    test

--- Backend/weeshoop/settings.py
+++ Backend/weeshoop/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static_files/'
-if not DEBUG:
+if DEBUG:
     STATIC_ROOT = os.path.join(BASE_DIR, "static_files")
 else:
     STATICFILES_DIRS = [
