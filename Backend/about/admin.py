678e2c94cb62fc4efbe182427ad449c4d9ba711a
osman-abi <osman.97.abi@gmail.com>
2021-04-07 09:30:35 +0400

    test

--- Backend/about/admin.py
+++ Backend/about/admin.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from .models import (
     About,
     OurShop,
-    Mission
+    Mission,
+    YourMessage
 )
 # Register your models here.
 admin.site.register(About)
 admin.site.register(OurShop)
 admin.site.register(Mission)
+admin.site.register(YourMessage)
--- Backend/order/admin.py
+++ Backend/order/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Invoice, MyOrders
 
+admin.site.register(Invoice)
+admin.site.register(MyOrders)
 # Register your models here.
